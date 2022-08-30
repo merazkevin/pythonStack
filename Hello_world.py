8443bead68e2bfcfdbfe26da54c7a2d54e7b2fd4
merazkevin <merazkevin@yahoo.com>
2022-08-29 18:16:37 -0800

    yeee

---
+++ Hello_world.py
@@ -0,0 +1,17 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print("Noelle"),	# with a comma
+print( "Hello " + name )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+
+name = 23
+print(name),	# with a comma
+# print( your code here )	# with a +	-- this one should give us an error!
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to Eat" + fave_food1 + " and " + fave_food2 ) # with .format()
+# print( your code here ) # with an f string
+
